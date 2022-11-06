e3083fae4923327de97ca3c2bd068b097a0f0320
Horta <Horta@DESKTOP-DQM7GVE>
2022-11-06 19:45:35 +0200

    Добавьте файлы проекта.

---
+++ 5.1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Миронова Анастасия 22-ИСП-2/1
+//5.1 Вариант 4
+//Уровень Сложный
+
+char[] str = new char[11];
+str = Console.ReadLine().ToCharArray();
+
+
+int a = 1;
+for (int i = 0; i < str.Length; i++)
+{
+    Console.WriteLine(str[i] + " ");
+    a *= (int)(Char.GetNumericValue(str[i]));
+
+}
+Console.WriteLine(a);
